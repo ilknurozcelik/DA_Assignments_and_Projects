7cdce89e156d281239a8834fa841b91fa6f79954
ilknurozcelik <ilknurozcelik2016@gmail.com>
2022-06-08 14:30:19 +0300

    SQL_Assignment_2 file was modified.

--- 01-Assignments/05_MS_SQL_Assignments/SQL_Assignment_2.sql
+++ 01-Assignments/05_MS_SQL_Assignments/SQL_Assignment_2.sql
@@ -1,9 +1,23 @@
-/* 2TB Red 5400 rpm SATA III 3.5 Internal NAS HDD 
-isimli ürünü alan müþteri bilgileri*/
--- BU ANA TABLO
---Bu tablo ile diðer tablolarý(diðer ürünleri alýp almadýðýný belirten tablolarý)
--- LEFT JOIN ile birleþtirerek ilerleyebiliriz.
-SELECT SC.customer_id, SC.first_name, SC.last_name, PP.product_name
+/*ASSÝGNMENT ÇÖZÜMÜ 1: Burada izlenen adýmlar aþaðýdaki gibidir:
+
+1. 2TB Red 5400 rpm SATA III 3.5 Internal NAS HDD 
+isimli ürünü alan müþteri bilgilerini içeren bir tablo oluþturulmuþ
+ve bu tablo ANA TABLO olarak kabul edilmiþtir.
+
+2. Diðer ürünler için de ayný þekilde tablolar oluþturulmuþ ve bunlar da sýrasýyla
+FIRST_PRODUCT
+SECON_PRODUCT
+THIRD_PRODUCT
+tablolarý olarak kabul edilmiþtir.
+
+3. ANA TABLO ile diðer ürün tablolarý LEFT JOIN  ile birleþtirilmiþtir.
+
+4. Elde edilen tablodaki ürün sütunlarýnda yer alan NULL deðerler ve ürün isimleri
+'NO' ve 'YES' ile deðiþtirilerek ANA tabloda belirtilen ürünü alan kiþilerin diðer üç
+ürünü alýp almadýðýný gösteren tablo elde edilmiþtir*/
+
+/* ANA TABLO */
+SELECT DISTINCT SC.customer_id, SC.first_name, SC.last_name, PP.product_name
 FROM product.product AS PP
 INNER JOIN sale.order_item AS SOI
 ON PP.product_id = SOI.product_id
@@ -11,13 +25,11 @@ INNER JOIN sale.orders AS SO
 ON SO.order_id = SOI.order_id
 INNER JOIN sale.customer AS SC
 ON SC.customer_id = SO.customer_id
-WHERE PP.product_name = '2TB Red 5400 rpm SATA III 3.5 Internal NAS HDD'
+WHERE PP.product_name LIKE '2TB Red 5400 rpm SATA III 3.5 Internal NAS HDD'
 ORDER BY SC.customer_id
 
-
 /* FIRST PRODUCT ('Polk Audio - 50 W Woofer - Black') TABLOSU*/
-
-SELECT SC.customer_id, SC.first_name, SC.last_name, PP.product_name AS FIRST_PRODUCT
+SELECT DISTINCT SC.customer_id, SC.first_name, SC.last_name, PP.product_name AS FIRST_PRODUCT
 FROM product.product AS PP
 INNER JOIN sale.order_item AS SOI
 ON PP.product_id = SOI.product_id
@@ -29,8 +41,7 @@ WHERE PP.product_name = 'Polk Audio - 50 W Woofer - Black'
 ORDER BY SC.customer_id
 
 /* SECOND PRODUCT ('SB-2000 12 500W Subwoofer (Piano Gloss Black)') TABLOSU*/
-
-SELECT SC.customer_id, SC.first_name, SC.last_name, PP.product_name AS SECOND_PRODUCT
+SELECT DISTINCT SC.customer_id, SC.first_name, SC.last_name, PP.product_name AS SECOND_PRODUCT
 FROM product.product AS PP
 INNER JOIN sale.order_item AS SOI
 ON PP.product_id = SOI.product_id
@@ -42,8 +53,7 @@ WHERE PP.product_name = 'SB-2000 12 500W Subwoofer (Piano Gloss Black)'
 ORDER BY SC.customer_id
 
 /* THIRD PRODUCT ('SB-2000 12 500W Subwoofer (Piano Gloss Black)') TABLOSU*/
-
-SELECT SC.customer_id, SC.first_name, SC.last_name, PP.product_name AS THIRD_PRODUCT
+SELECT DISTINCT SC.customer_id, SC.first_name, SC.last_name, PP.product_name AS THIRD_PRODUCT
 FROM product.product AS PP
 INNER JOIN sale.order_item AS SOI
 ON PP.product_id = SOI.product_id
@@ -54,62 +64,60 @@ ON SC.customer_id = SO.customer_id
 WHERE PP.product_name = 'Virtually Invisible 891 In-Wall Speakers (Pair)' 
 ORDER BY SC.customer_id
 
-/* ANA TABLO ÝLE 1., 2., 3. PRODUCT TABLOLARININ LEFT JOIN ÝLE BÝRLEÞTÝRÝLMESÝ*/
+/* ANA TABLO ÝLE 1., 2., 3. PRODUCT TABLOLARININ LEFT JOIN ÝLE BÝRLEÞTÝRÝLMESÝ
+VE NULL ÝFADELERÝNÝN STRÝNG FONKSÝYONLARLA DÜZELTÝLMESÝ*/
 
-SELECT A.customer_id, A.first_name, A.last_name, FP.FIRST_PRODUCT, SP.SECOND_PRODUCT, TP.THIRD_PRODUCT
-FROM (SELECT SC.customer_id, SC.first_name, SC.last_name, PP.product_name
-FROM product.product AS PP
-INNER JOIN sale.order_item AS SOI
-ON PP.product_id = SOI.product_id
-INNER JOIN sale.orders AS SO
-ON SO.order_id = SOI.order_id
-INNER JOIN sale.customer AS SC
-ON SC.customer_id = SO.customer_id
-WHERE PP.product_name = '2TB Red 5400 rpm SATA III 3.5 Internal NAS HDD') AS A
-LEFT JOIN (SELECT SC.customer_id, SC.first_name, SC.last_name, PP.product_name AS FIRST_PRODUCT
-FROM product.product AS PP
-INNER JOIN sale.order_item AS SOI
-ON PP.product_id = SOI.product_id
-INNER JOIN sale.orders AS SO
-ON SO.order_id = SOI.order_id
-INNER JOIN sale.customer AS SC
-ON SC.customer_id = SO.customer_id
-WHERE PP.product_name = 'Polk Audio - 50 W Woofer - Black') as FP
-ON A.customer_id = FP.customer_id
-LEFT JOIN (SELECT SC.customer_id, SC.first_name, SC.last_name, PP.product_name AS SECOND_PRODUCT
-FROM product.product AS PP
-INNER JOIN sale.order_item AS SOI
-ON PP.product_id = SOI.product_id
-INNER JOIN sale.orders AS SO
-ON SO.order_id = SOI.order_id
-INNER JOIN sale.customer AS SC
-ON SC.customer_id = SO.customer_id
-WHERE PP.product_name = 'SB-2000 12 500W Subwoofer (Piano Gloss Black)') AS SP
-ON A.customer_id=SP.customer_id
-LEFT JOIN (SELECT SC.customer_id, SC.first_name, SC.last_name, PP.product_name AS THIRD_PRODUCT
-FROM product.product AS PP
-INNER JOIN sale.order_item AS SOI
-ON PP.product_id = SOI.product_id
-INNER JOIN sale.orders AS SO
-ON SO.order_id = SOI.order_id
-INNER JOIN sale.customer AS SC
-ON SC.customer_id = SO.customer_id
-WHERE PP.product_name = 'Virtually Invisible 891 In-Wall Speakers (Pair)') AS TP
-ON A.customer_id=TP.customer_id
+SELECT A.customer_id, A.first_name, A.last_name,
+	REPLACE(ISNULL(FP.FIRST_PRODUCT, 'NO'), 'Polk Audio - 50 W Woofer - Black', 'YES') AS FIRST_PRODUCT,
+	REPLACE(ISNULL(SP.SECOND_PRODUCT, 'NO'), 'SB-2000 12 500W Subwoofer (Piano Gloss Black)', 'YES') AS SECOND_PRODUCT,
+	REPLACE(ISNULL(TP.THIRD_PRODUCT, 'NO'), 'Virtually Invisible 891 In-Wall Speakers (Pair)', 'YES') AS THIRD_PRODUCT
+FROM (SELECT DISTINCT SC.customer_id, SC.first_name, SC.last_name, PP.product_name
+	FROM product.product AS PP
+	INNER JOIN sale.order_item AS SOI 
+	ON PP.product_id = SOI.product_id
+	INNER JOIN sale.orders AS SO
+	ON SO.order_id = SOI.order_id
+	INNER JOIN sale.customer AS SC
+	ON SC.customer_id = SO.customer_id
+	WHERE PP.product_name = '2TB Red 5400 rpm SATA III 3.5 Internal NAS HDD') AS A
+LEFT JOIN (SELECT DISTINCT SC.customer_id, SC.first_name, SC.last_name, PP.product_name AS FIRST_PRODUCT
+	FROM product.product AS PP
+	INNER JOIN sale.order_item AS SOI
+	ON PP.product_id = SOI.product_id
+	INNER JOIN sale.orders AS SO
+	ON SO.order_id = SOI.order_id
+	INNER JOIN sale.customer AS SC
+	ON SC.customer_id = SO.customer_id
+	WHERE PP.product_name = 'Polk Audio - 50 W Woofer - Black') as FP
+	ON A.customer_id = FP.customer_id
+LEFT JOIN (SELECT DISTINCT SC.customer_id, SC.first_name, SC.last_name, PP.product_name AS SECOND_PRODUCT
+	FROM product.product AS PP
+	INNER JOIN sale.order_item AS SOI
+	ON PP.product_id = SOI.product_id
+	INNER JOIN sale.orders AS SO
+	ON SO.order_id = SOI.order_id
+	INNER JOIN sale.customer AS SC
+	ON SC.customer_id = SO.customer_id
+	WHERE PP.product_name = 'SB-2000 12 500W Subwoofer (Piano Gloss Black)') AS SP
+	ON A.customer_id=SP.customer_id
+LEFT JOIN (SELECT DISTINCT SC.customer_id, SC.first_name, SC.last_name, PP.product_name AS THIRD_PRODUCT
+	FROM product.product AS PP
+	INNER JOIN sale.order_item AS SOI
+	ON PP.product_id = SOI.product_id
+	INNER JOIN sale.orders AS SO
+	ON SO.order_id = SOI.order_id
+	INNER JOIN sale.customer AS SC
+	ON SC.customer_id = SO.customer_id
+	WHERE PP.product_name = 'Virtually Invisible 891 In-Wall Speakers (Pair)') AS TP
+	ON A.customer_id=TP.customer_id
 ORDER BY A.customer_id
 
-/* VÝEW TANIMLAYARAK AYNI KODU TEKRAR YAZALIM.*/
 
+/* ASSÝGNMENT ÇÖZÜMÜ 2: VÝEW TANIMLAYARAK*/
 
-
-
-
-
-
-/* 2TB Red 5400 rpm SATA III 3.5 Internal NAS HDD 
-isimli ürünü alan müþterilerin customer ID leri*/
-
-SELECT SC.customer_id
+/* VIEW OLUÞTURMA */
+CREATE VIEW JOINT_TABLE AS
+SELECT DISTINCT SC.customer_id, SC.first_name, SC.last_name, PP.product_name
 FROM product.product AS PP
 INNER JOIN sale.order_item AS SOI
 ON PP.product_id = SOI.product_id
@@ -117,73 +125,40 @@ INNER JOIN sale.orders AS SO
 ON SO.order_id = SOI.order_id
 INNER JOIN sale.customer AS SC
 ON SC.customer_id = SO.customer_id
-WHERE PP.product_name = '2TB Red 5400 rpm SATA III 3.5 Internal NAS HDD'
-ORDER BY SC.customer_id
 
-/* 2TB Red 5400 rpm SATA III 3.5 Internal NAS HDD 
-isimli ürünü alan müþterilerin aldýklarý diðer ürün bilgilerinin getirilmesi*/
+-- VIEW KULLANIMI
+/* JOINT_TABLE VÝEW'Ü KULLANILARAK ANA TABLONUN OLUÞTURULMASI */
+SELECT * FROM JOINT_TABLE
+WHERE product_name LIKE '2TB Red 5400 rpm SATA III 3.5 Internal NAS HDD'
 
-SELECT SC.customer_id, SC.first_name, SC.last_name, PP.product_name
-FROM product.product AS PP
-INNER JOIN sale.order_item AS SOI
-ON PP.product_id = SOI.product_id
-INNER JOIN sale.orders AS SO
-ON SO.order_id = SOI.order_id
-INNER JOIN sale.customer AS SC
-ON SC.customer_id = SO.customer_id
-WHERE PP.product_name
-	IN('Polk Audio - 50 W Woofer - Black', 'SB-2000 12 500W Subwoofer (Piano Gloss Black)', 'Virtually Invisible 891 In-Wall Speakers (Pair)')
-GROUP BY SC.customer_id, SC.first_name, SC.last_name, PP.product_name
-HAVING SC.customer_id IN(SELECT SC.customer_id
-FROM product.product AS PP
-INNER JOIN sale.order_item AS SOI
-ON PP.product_id = SOI.product_id
-INNER JOIN sale.orders AS SO
-ON SO.order_id = SOI.order_id
-INNER JOIN sale.customer AS SC
-ON SC.customer_id = SO.customer_id
-WHERE PP.product_name = '2TB Red 5400 rpm SATA III 3.5 Internal NAS HDD')
-ORDER BY SC.customer_id
+/* JOINT_TABLE VÝEW'Ü KULLANILARAK FIRST_PRODUCT TABLOSUNUN OLUÞTURULMASI */
+SELECT customer_id, product_name AS FIRST_PRODUCT FROM JOINT_TABLE
+WHERE product_name LIKE 'Polk Audio - 50 W Woofer - Black'
 
-/* UNION ile faklý sorgu sonuçlarýnýn birleþitirilmesi*/
-/* NOT: UNION ile birleþtirilecek sonuçlarýn ayný uzunlukta olmasý gerekir.*/
+/* JOINT_TABLE VÝEW'Ü KULLANILARAK SECOND_PRODUCT TABLOSUNUN OLUÞTURULMASI */
+SELECT customer_id, product_name AS SECOND_PRODUCT FROM JOINT_TABLE
+WHERE product_name LIKE '2TB Red 5400 rpm SATA III 3.5 Internal NAS HDD'
 
-SELECT SC.customer_id, SC.first_name, SC.last_name
-FROM product.product AS PP
-INNER JOIN sale.order_item AS SOI
-ON PP.product_id = SOI.product_id
-INNER JOIN sale.orders AS SO
-ON SO.order_id = SOI.order_id
-INNER JOIN sale.customer AS SC
-ON SC.customer_id = SO.customer_id
-WHERE PP.product_name = '2TB Red 5400 rpm SATA III 3.5 Internal NAS HDD'
-UNION ALL
-SELECT PP.product_name AS first_product
-FROM product.product AS PP
-INNER JOIN sale.order_item AS SOI
-ON PP.product_id = SOI.product_id
-INNER JOIN sale.orders AS SO
-ON SO.order_id = SOI.order_id
-INNER JOIN sale.customer AS SC
-ON SC.customer_id = SO.customer_id
-WHERE PP.product_name
-	IN('Polk Audio - 50 W Woofer - Black')
-GROUP BY SC.customer_id, SC.first_name, SC.last_name, PP.product_name
-HAVING SC.customer_id IN(SELECT SC.customer_id
-FROM product.product AS PP
-INNER JOIN sale.order_item AS SOI
-ON PP.product_id = SOI.product_id
-INNER JOIN sale.orders AS SO
-ON SO.order_id = SOI.order_id
-INNER JOIN sale.customer AS SC
-ON SC.customer_id = SO.customer_id
-WHERE PP.product_name = '2TB Red 5400 rpm SATA III 3.5 Internal NAS HDD')
-ORDER BY SC.customer_id
+/* JOINT_TABLE VÝEW'Ü KULLANILARAK THIRD_PRODUCT TABLOSUNUN OLUÞTURULMASI */
+SELECT customer_id, product_name AS THIRD_PRODUCT FROM JOINT_TABLE
+WHERE product_name LIKE '2TB Red 5400 rpm SATA III 3.5 Internal NAS HDD'
 
-
-
-
-
-
-
-
+/* VÝEW KULLANILARAK OLUÞTURULAN ANA TABLO ÝLE DÝÐER ÜRÜN TABLOLARININ
+LEFT JOIN ÝLE BÝRLEÞTÝRÝLMESÝ VE STRING FONKSÝYONLAR ÝLE ÜRÜN 
+KOLONLARININ BÝLGÝLERÝNÝN DÜZENLENMESÝ */
+SELECT A.customer_id, A.first_name, A.last_name,
+	REPLACE(ISNULL(FP.FIRST_PRODUCT, 'NO'),  'Polk Audio - 50 W Woofer - Black', 'YES') AS FIRST_PRODUCT,
+	REPLACE(ISNULL(SP.SECOND_PRODUCT, 'NO'), 'SB-2000 12 500W Subwoofer (Piano Gloss Black)', 'YES') AS SECOND_PRODUCT,
+	REPLACE(ISNULL(TP.THIRD_PRODUCT, 'NO'), 'Virtually Invisible 891 In-Wall Speakers (Pair)', 'YES') AS THIRD_PRODUCT
+FROM (SELECT * FROM JOINT_TABLE
+	WHERE product_name LIKE '2TB Red 5400 rpm SATA III 3.5 Internal NAS HDD') AS A
+LEFT JOIN (SELECT customer_id, product_name AS FIRST_PRODUCT FROM JOINT_TABLE
+		WHERE product_name LIKE 'Polk Audio - 50 W Woofer - Black') AS FP
+		ON A.customer_id = FP.customer_id
+LEFT JOIN (SELECT customer_id, product_name AS SECOND_PRODUCT FROM JOINT_TABLE
+		WHERE product_name LIKE 'SB-2000 12 500W Subwoofer (Piano Gloss Black)') AS SP
+		ON A.customer_id = SP.customer_id
+LEFT JOIN (SELECT customer_id, product_name AS THIRD_PRODUCT FROM JOINT_TABLE
+		WHERE product_name LIKE 'Virtually Invisible 891 In-Wall Speakers (Pair)') AS TP
+		ON A.customer_id = TP.customer_id
+ORDER BY customer_id
